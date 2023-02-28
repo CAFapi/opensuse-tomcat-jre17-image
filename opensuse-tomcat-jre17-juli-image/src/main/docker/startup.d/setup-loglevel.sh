6f5b8af09c2cfce1282e023216b004ab0e38b20c
CORPDOM\TBroadbent <tony.broadbent@microfocus.com>
2023-02-28 14:28:12 +0000

    Changing to open text header

--- opensuse-tomcat-jre17-juli-image/src/main/docker/startup.d/setup-loglevel.sh
+++ opensuse-tomcat-jre17-juli-image/src/main/docker/startup.d/setup-loglevel.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+#
+# Copyright 2022-2023 Open Text.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # This script replaces the log level value in logging.properties if environment variable CAF_LOG_LEVEL is set
 #
 #Declare and initialize associative array. This is a mapping of log4j2 levels with that of java.util.logging
